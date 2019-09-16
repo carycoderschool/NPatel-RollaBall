2fa0ae0eb5e64e9db961525f8a365dd31e9276a8
carycoderschool <42698327+carycoderschool@users.noreply.github.com>
2019-09-16 17:59:36 -0400

    Initial commit

---
+++ Assets/scripts/NewBehaviourScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+    }
+
+}
